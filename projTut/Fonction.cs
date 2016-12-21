53a2c6463b2e67eb4bdf871ff0fc5cc7ec52d7db
Maël Belval <mael7@free.fr>
2016-12-21 16:54:44 +0100

    Ajout commentaire fonction

--- projTut/Fonction.cs
+++ projTut/Fonction.cs
@@ -8,8 +8,18 @@ namespace projTut
 {
     class Fonction : Procedure
     {
+        /// <summary>
+        /// Une fonction est une procédure ou la seul différence est qu'elle a une sortie
+        /// </summary>
         private Variable retour;
 
+        /// <summary>
+        /// Constructeur par défaut, reprise du constructeur de procédure avec ajout de variable de retour
+        /// </summary>
+        /// <param name="listeParametre">liste param lors de l'appel</param>
+        /// <param name="listeInstruction">liste instruction executé dans fonction</param>
+        /// <param name="nom">nom de la fonction</param>
+        /// <param name="retour">variable de retour</param>
         public Fonction(List<Parametre> listeParametre, List<Instruction> listeInstruction, string nom, Variable retour)
             :base(listeParametre, listeInstruction, nom)
         {
