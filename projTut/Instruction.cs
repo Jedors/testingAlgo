6a41827cb47bffba10a2801c90fd66507fb699f8
PERRIN Geoffrey <perrin.geoffrey.1@gmail.com>
2016-12-21 18:49:21 +0100

    ragequit

--- projTut/Instruction.cs
+++ projTut/Instruction.cs
@@ -9,13 +9,8 @@ namespace projTut
 {
     class Instruction
     {
-
-        string line;
-        Fonction fonc;
-        Procedure proc;
-        
-   
-
+        string Line;
+        Variable AssigneA;
 
         public Instruction(Instruction instruction)
         {
@@ -24,105 +19,38 @@ namespace projTut
 
         public Instruction(string line)
         {
-            this.line = line;
+            this.Line = line;
+            AnalyseInstruction();
         }
 
         public void AnalyseInstruction()
-        {
-
-            
+        {       
             string ppv = Properties.Resources.ppv;
-            int debutpara = line.IndexOf("(");
+            int debutpara = Line.IndexOf("(");
             if ( debutpara != -1)
             {
                 Boolean fonc = false;
-                int debutfonc= line.IndexOf(ppv);   
+                int debutfonc= Line.IndexOf(ppv);
                 if (debutfonc != -1)
                 {
-                    string variablenom = line.Substring(0,debutfonc);
-                    line = line.Substring(debutfonc+ppv.Length);
-                    
+                    string variablenom = Line.Substring(0, debutfonc);
+                    Line = Line.Substring(debutfonc + ppv.Length);
                     fonc = true;
+                    new Fonction(Line);
                 }
 
 
-                debutpara = line.IndexOf("(");
-                int finpara = line.LastIndexOf(")");
-                int length = finpara - debutpara;
-                Console.WriteLine(line);
-                string para = line.Substring(debutpara+1, length-1);
-                Console.WriteLine(para);
-                string para1 = "";
-                int inproc = 0;
-                int i= 0;
-                int j = 1;
-                List<Parametre> listeParametre = new List<Parametre>();
-                for (i = 0; i < para.Length; i++)
-                {   
-                    if (para[i] == '(')
-                    {
-                        inproc ++;
-                    }
 
-                    else if (para[i] == ')')
-                    {
-                        inproc --;
-                    }
-
-                    if ((para[i] == ',') && (inproc==0))
-                    {    
-                        listeParametre.Add(determinePara(para1));
-                        para1 = "";
-                        j++;
-                    }
-
-                    else
-                    {
-                        para1 += para[i];
-                    }
+                else
+                {
+                    Console.WriteLine(Line);
+                    new Procedure(Line);
                 }
-                
-                listeParametre.Add(determinePara(para1));
-                proc = new Procedure(listeParametre, determineNom(line, debutpara));
-                Console.WriteLine(proc);
-            }           
+            }
+
+                  
         }
 
 
-
-        private Parametre determinePara(string para)
-        {
-            Parametre parametre=null;
-            if (para.IndexOf(">(")!=-1 || para.IndexOf("<(") != -1 || para.IndexOf("=(") != -1 || para.IndexOf(")>") != -1 || para.IndexOf(")<") != -1 || para.IndexOf(")=") != -1)
-            {
-                parametre = new Parametre(new Type(Type.types.BOOLEAN),Parametre.PassagePar.VALEUR,para);
-                Instruction ins = new Instruction(para);
-                ins.AnalyseInstruction();
-            }
-
-            else if (para.IndexOf('(') != -1)
-            {
-                parametre = new Parametre(new Type(Type.types.UNKNOWN), Parametre.PassagePar.VALEUR, para);
-                Instruction ins = new Instruction(para);
-                ins.AnalyseInstruction();
-            }
-
-            else
-            {
-                parametre = new Parametre(new Type(Type.types.UNKNOWN), Parametre.PassagePar.UNKNOWN, para);
- 
-            }
-
-
-
-            return parametre;
-        }
-
-        private string determineNom(string proc,int debutpara)
-        {
-
-                return proc.Substring(0, debutpara);
-            
-        }
     }
 }
--- projTut/Procedure.cs
+++ projTut/Procedure.cs
@@ -31,12 +31,16 @@ namespace projTut
             listeInstruction.Add(new Instruction(instruction));
         }
 
+        public Procedure(string line)
+        {
+            AnalyseProcedure(line);
+        }
         
         public override string ToString()
         {
             int i = 1;
             string retour;
-            retour = "Nom de la fonction : " + this.nom;
+            retour = "Nom de la procédure : " + this.nom;
             
             if(listeParametre != null)
             {
@@ -46,10 +50,85 @@ namespace projTut
                     i++;
                     retour += Para.ToString();
                     retour += "\n";
-                }
-                
+                } 
             }
             return retour;
+        }
+
+        private void AnalyseProcedure(string line)
+        {
+            int debutpara = line.IndexOf("(");
+            int finpara = line.LastIndexOf(")");
+            int length = finpara - debutpara;
+            Console.WriteLine(line);
+            string para = line.Substring(debutpara + 1, length - 1);
+            Console.WriteLine(para);
+            string para1 = "";
+            int inproc = 0;
+            int i = 0;
+            int j = 1;
+            listeParametre = new List<Parametre>();
+            for (i = 0; i < para.Length; i++)
+            {
+                if (para[i] == '(')
+                {
+                    inproc++;
+                }
+
+                else if (para[i] == ')')
+                {
+                    inproc--;
+                }
+
+                if ((para[i] == ',') && (inproc == 0))
+                {
+                    listeParametre.Add(determinePara(para1));
+                    para1 = "";
+                    j++;
+                }
+
+                else
+                {
+                    para1 += para[i];
+                }
+            }
+
+            listeParametre.Add(determinePara(para1));
+            Procedure proc= new Procedure(listeParametre, determineNom(line, debutpara));
+            Console.WriteLine(proc);
+        }
+
+
+        protected Parametre determinePara(string para)
+        {
+            Parametre parametre = null;
+            if (para.IndexOf(">(") != -1 || para.IndexOf("<(") != -1 || para.IndexOf("=(") != -1 || para.IndexOf(")>") != -1 || para.IndexOf(")<") != -1 || para.IndexOf(")=") != -1)
+            {
+                parametre = new Parametre(new Type(Type.types.BOOLEAN), Parametre.PassagePar.VALEUR, para);
+                Instruction ins = new Instruction(para);
+                ins.AnalyseInstruction();
+            }
+
+            else if (para.IndexOf('(') != -1)
+            {
+                parametre = new Parametre(new Type(Type.types.UNKNOWN), Parametre.PassagePar.VALEUR, para);
+                Instruction ins = new Instruction(para);
+                ins.AnalyseInstruction();
+            }
+
+            else
+            {
+                parametre = new Parametre(new Type(Type.types.UNKNOWN), Parametre.PassagePar.UNKNOWN, para);
+
+            }
+
+            return parametre;
+        }
+
+        protected string determineNom(string proc, int debutpara)
+        {
+
+            return proc.Substring(0, debutpara);
 
         }
     }
